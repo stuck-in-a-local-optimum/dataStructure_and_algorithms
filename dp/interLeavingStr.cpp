abcd6c32141efbd2e801e9112bc3f43660ad59c8
Ajeet Yadav <ajeet19010@iiitd.ac.in>
2021-05-31 04:04:05 +0530

    interleaving strings

---
+++ dp/interLeavingStr.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+
+
+unordered_map<string, bool> mem;
+
+
+
+          bool check( string s1, string s2, string s3, int len1, int len2, int len3, int p1, int p2, int p3){
+              if(p3==len3){         //if s3 has been totally passed 
+                  return (p1==len1 and p2==len2) ? true : false;  //check same is true for s1 and s2
+              }
+
+            string key = to_string(p1)+"*"+to_string(p2)+"*"+to_string(p3);
+
+            if(mem.find(key)!=mem.end()){
+                return mem[key];   //return the ans if already calculated
+            }
+
+            if(p1 == len1){ //if first string is completed
+                    if( s2[p2] == s3[p3]){
+                       return mem[key] = check(s1, s2, s3, len1, len2, len3, p1, p2+1, p3+1);
+                    }
+                    return mem[key] = false;
+            }
+
+
+             if(p2 == len2){ //if first string is completed
+                    if( s1[p1] == s3[p3]){
+                       return mem[key] = check(s1, s2, s3, len1, len2, len3, p1+1, p2, p3+1);
+                    }
+                    return mem[key] = false;
+            }
+
+
+            bool one = false;
+            bool two = false;
+
+            if(s1[p1] == s3[p3])
+                one =  check(s1, s2, s3, len1, len2, len3, p1+1, p2, p3+1);
+
+             if(s2[p2] == s3[p3])
+                two =  check(s1, s2, s3, len1, len2, len3, p1, p2+1, p3+1);
+
+
+
+            
+        return mem[key] = one || two;
+
+          }
+
+
+
+
+
+
+
+
+
+
+bool isInterleaving( string s1, string s2, string s3){
+    int len1 = s1.length();
+    int len2 = s2.length();
+    int len3 = s3.length();
+
+    if(len3 != len1 + len2){
+        return false;
+    }
+
+    return check(  s1, s2, s3, len1,  len2,  len3, 0, 0, 0);
+
+}
+
+
+
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
+
+
+
+
